1a251058c83ca7c43046e816d969bafb9aa39306
Sae <winny9641@gmail.com>
2022-05-31 01:34:09 -0400

    Template strings method added

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,11 @@
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    return console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    //return console.log("✅✅✅ Assertion Passed: " + actual + " === " + expected);
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    //return console.log("🛑🛑🛑 Assertion Failed: " + actual + " !== " + expected);
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
