e8115436a8288eb4abc69eff3b1740c44c294686
Sae <winny9641@gmail.com>
2022-05-31 21:44:38 -0400

    asserArrayEqual function added

---
+++ assertArraysEqual.js
@@ -0,0 +1,40 @@
+
+const eqArrays = function(a1,a2) {
+  // length check, list check
+
+  if (!Array.isArray(a1) && !Array.isArray(a2)) {
+    return false;
+  }
+
+  if (a1.length !== a2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < a1.length; i++) {
+  
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+
+};
+
+const assertArraysEqual = function(a1,a2) {
+  if (!eqArrays(a1,a2)) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${a1}] !== [${a2}]`);
+  } else if (eqArrays(a1,a2)) {
+    console.log(`✅✅✅ Assertion Passed: [${a1}] === [${a2}]`);
+  }
+
+};
+
+
+//Test Case
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,3,3]);
+assertArraysEqual([1,2,3],[1,2,'hallo']);
+assertArraysEqual([1,2,3],[1,2]);
+assertArraysEqual([],[]);
--- tail.js
+++ tail.js
@@ -29,3 +29,4 @@ assertEqual(words.length, 3); // original array should still have 3 elements!
 
 
 console.log(tail([]));
+
